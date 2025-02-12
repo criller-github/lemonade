7d874d9d864bcbd3c18ea062988da022ce7b7a39
“criller-github” <chri38nq@edu.zealand.dk>
2025-02-12 16:30:53 +0100

    første push

---
+++ lemonadeStand/src/App.jsx
@@ -0,0 +1,13 @@
+
+import LemonadeStand from "./LemonadeStand";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LemonadeStand />
+    </div>
+  );
+}
+
+export default App
